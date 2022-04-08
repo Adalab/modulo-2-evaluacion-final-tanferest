2167f9e1d59ab932a91dd3aaaf9e87341675e6c3
tanferest <tanferest@gmail.com>
2022-04-08 17:23:07 +0200

    create function to paint fav drinks

--- src/js/main.js
+++ src/js/main.js
@@ -7,25 +7,26 @@ const inputSearch = document.querySelector('.js_input_search');
 const searchBtn = document.querySelector('.js_button');
 const searchResult = document.querySelector('.js_result_list');
 const favoritesList = document.querySelector('.js_fav_list');
+const altImage = 'https://via.placeholder.com/210x295/ffffff/666666/?text=cocktail';
 let resultsList = [];
-let htmlResult = '';
 let favList = [];
-
 // ---- FUNCIONES
 
 // Pintar resultados de búsqueda
 function paintListResult() {
+  let htmlResult = '';
   for (const item of resultsList) {
     htmlResult += `<li class="results__item js_results_item" id=${item.idDrink}>`;
     htmlResult += `<h3 class="results__item--name">${item.strDrink}</h3>`;
     if (item.strDrinkThumb !== null) {
       htmlResult += `<img class="cocktail-img" src=${item.strDrinkThumb}>`;
     } else {
-      htmlResult += `<img class="cocktail-img" src=https://via.placeholder.com/210x295/ffffff/666666/?text=cocktail>`;
+      htmlResult += `<img class="cocktail-img" src=${altImage}>`;
     }
     htmlResult += `</li>`;
   }
   searchResult.innerHTML = htmlResult;
+  manageFavCocktails();
 }
 
 // Petición de los datos al api
@@ -38,7 +39,6 @@ function getCocktailData() {
       resultsList = data.drinks;
       console.log(resultsList);
       paintListResult();
-      manageFavCocktails();
     });
 }
 
@@ -65,26 +65,42 @@ function manageFavCocktails() {
 }
 
 function paintFavCocktails() {
-
-}
+  let htmlFav = '';
+  for (const favItem of favList) {
+    htmlFav += `<li class="results__item js_results_item" id=${favItem.idDrink}>`;
+    htmlFav += `<h3 class="results__item--name">${favItem.strDrink}</h3>`;
+    if (favItem.strDrinkThumb !== null) {
+      htmlFav += `<img class="cocktail-img" src=${favItem.strDrinkThumb}>`;
+    } else {
+      htmlFav += `<img class="cocktail-img" src=${altImage}>`;
+    }
+    htmlFav += `</li>`;
+  }
+  favoritesList.innerHTML = htmlFav;
+};
 
 function handleFavClick(event) {
   const selectedCocktail = event.currentTarget.id;
   console.log(selectedCocktail);
-  // Busco en el resultado de búsqueda el cóctel seleccionado
+  // Busco en el resultado de búsqueda el ELEMENTO seleccionado
   const clickedDrink = resultsList.find((fav) => {
     return fav.idDrink === selectedCocktail;
   });
+  // Compruebo si el elemento está en la lista de favoritos
   const favDrink = favList.findIndex((fav)=>{
     return fav.idDrink === selectedCocktail;
   });
+  // Si no lo está, lo añado a la lista y si lo está, lo elimino
   if (favDrink === -1) {
     favList.push(clickedDrink);
   } else {
     favList.splice(favDrink,1);
   }
   console.log(favList);
+  paintFavCocktails();
 }
 
 // ---- EVENTOS
 searchBtn.addEventListener('click', handleSearchClick);
+
+
